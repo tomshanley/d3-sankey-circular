7e5ffc9c23ce011219eb51e9a7276973b99101ef
Tom Shanley <tom.shanley@gmail.com>
2017-10-12 15:15:12 +1300

    Removed redundant sankeyPathArrows

--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
 export {default as sankeyCircular} from "./sankeyCircular.js";
 export {center as sankeyCenter, left as sankeyLeft, right as sankeyRight, justify as sankeyJustify} from "./align";
-//export {default as sankeyPath} from "./sankeyPath.js"; 
-//export {default as sankeyPathArrows} from "./sankeyPathArrows.js"; 
