c405e2149c05bb2e2b1cf9874be6fc35830bd305
@micahstubbs <micah.stubbs@gmail.com>
2018-05-29 13:42:03 -0700

    include d3-shape in globals under d3 global

--- rollup.config.js
+++ rollup.config.js
@@ -12,9 +12,15 @@ export default [
     input: 'src/index.js',
     output: {
       file: pkg.main,
-      name: 'd3SankeyCircular',
+      name: 'sankeyCircular',
       format: 'umd',
-      globals: ['d3-array', 'd3', 'd3-collection:d3', 'd3-shape:d3']
+      globals: {
+        'd3-collection': 'd3',
+        'd3-array': 'd3',
+        'd3-interpolate': 'd3',
+        'd3-path': 'd3',
+        'd3-shape': 'd3'
+      }
     },
     external,
     plugins: [
@@ -33,30 +39,19 @@ export default [
     input: 'src/index.js',
     output: [
       {
-        file: pkg.main,
-        format: 'es',
-        name: 'd3SankeyCircular',
-        globals: {
-          'd3-collection': 'd3',
-          'd3-array': 'd3',
-          'd3-interpolate': 'd3',
-          'd3-path': 'd3'
-        }
-      },
-      {
         file: pkg.module,
         format: 'es',
-        name: 'd3SankeyCircular',
+        name: 'sankeyCircular',
         globals: {
           'd3-collection': 'd3',
           'd3-array': 'd3',
           'd3-interpolate': 'd3',
-          'd3-path': 'd3'
+          'd3-path': 'd3',
+          'd3-shape': 'd3'
         }
       }
     ],
     external,
-
-    plugins: [babel(babel(babelrc()))]
+    plugins: [babel(babelrc())]
   }
 ]
